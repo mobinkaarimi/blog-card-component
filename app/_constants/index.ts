6f430b33296c7f82dd33c947a0dfead8b740ce99
mobinkaarimi <68745874+mobinkaarimi@users.noreply.github.com>
2024-01-01 22:48:10 +0330

    create[index.ts]-> create mock data

---
+++ app/_constants/index.ts
@@ -0,0 +1,46 @@
+export const blogCardList = [
+  {
+    id: 1,
+    authorAvatar: "/HeroImage.jpg",
+    picture: "/HeroImage.jpg",
+    title: "How I Built Web Development Challenges Website",
+    description:
+      "If you are looking for inspiration to build your own side project about your profile",
+    category: "Technology",
+    author: "Corey Lubin",
+    createTime: "4 day ago",
+  },
+  {
+    id: 2,
+    authorAvatar: "/HeroImage.jpg",
+    picture: "/HeroImage.jpg",
+    title: "How I Built Web Development Challenges Website",
+    description:
+      "If you are looking for inspiration to build your own side project about your profile",
+    category: "Technology",
+    author: "Corey Lubin",
+    createTime: "4 day ago",
+  },
+  {
+    id: 3,
+    authorAvatar: "/HeroImage.jpg",
+    picture: "/HeroImage.jpg",
+    title: "How I Built Web Development Challenges Website",
+    description:
+      "If you are looking for inspiration to build your own side project about your profile",
+    category: "Technology",
+    author: "Corey Lubin",
+    createTime: "4 day ago",
+  },
+  {
+    id: 4,
+    authorAvatar: "/HeroImage.jpg",
+    picture: "/HeroImage.jpg",
+    title: "How I Built Web Development Challenges Website",
+    description:
+      "If you are looking for inspiration to build your own side project about your profile",
+    category: "Technology",
+    author: "Corey Lubin",
+    createTime: "4 day ago",
+  },
+];
