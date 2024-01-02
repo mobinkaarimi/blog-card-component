81eb4bd11b38e57557904285fbbd8e84d7001716
mobinkaarimi <68745874+mobinkaarimi@users.noreply.github.com>
2024-01-02 22:38:54 +0330

    create[cardItem.ts]-> create file & type of cardItem

---
+++ app/@types/cardItem.ts
@@ -0,0 +1,10 @@
+export interface cardItemType {
+    id: number;
+    title: string;
+    description: string;
+    category: string;
+    author: string;
+    createTime: string;
+    picture: string;
+    authorAvatar: string
+}
