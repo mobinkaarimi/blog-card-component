d6c0044327c3a47ae3d9df3bbd7ac32f5267f4cd
mobinkaarimi <68745874+mobinkaarimi@users.noreply.github.com>
2024-01-02 23:19:01 +0330

    create[DescriptionShorter]-> this component create a short of description and had a logic you can set parameter for show each text to show and after add ...

---
+++ app/_components/DescriptionShorter/index.tsx
@@ -0,0 +1,18 @@
+import { DescriptionShorterType, checkLetterType } from "@types/cardItem";
+
+export default function DescriptionShorter({ text, maxLetter }: DescriptionShorterType)  {
+  function checkLetterLong(paragraph: string,maxLetter: number) {
+    let txt = "";
+    if (paragraph.split(" ").length > maxLetter) {
+      for (let textItem = 0; textItem < maxLetter; textItem++) {
+        txt += ` ${paragraph.split(" ")[textItem]}`;
+      }
+      text += " ...";
+    } else {
+      txt = paragraph;
+    }
+    return txt;
+  }
+  let output = checkLetterLong(text,maxLetter);
+  return <p className="text-gray-500">{output}</p>;
+}
