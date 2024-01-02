4bfacd4ba7fda2d83d80877ce8b0e0546ffa9259
mobinkaarimi <68745874+mobinkaarimi@users.noreply.github.com>
2024-01-02 22:40:04 +0330

    create[BlogCardItem]-> create component for show each blog card

---
+++ app/_components/BlogCard/BlogCardItem/index.tsx
@@ -0,0 +1,41 @@
+
+import Image from "next/image";
+import { cardItemType } from "@types/cardItem";
+export default function BlogCardItem(props: cardItemType) {
+  const {title, description, category, author, createTime, picture, authorAvatar, id} = props;
+  console.log(picture)
+  return (
+    <div className="rounded-xl relative overflow-hidden bg-white"
+    >
+      <Image
+        alt={title}
+        src={picture}
+        width={0}
+        height={0}
+        sizes="100vw"
+        style={{ width: "100%", height: "auto" }}
+      />
+
+      <div className="flex flex-col space-x-1 px-4 py-4 flex-nowrap gap-y-3 my-2">
+        <div>
+          <h2 className="text-sm font-normal text-white bg-orange-500 inline py-1 px-3 rounded-xl">{category}</h2>
+        </div>
+        <h2 className="text-xl font-medium">{title}</h2>
+
+        <div className="flex flex-row space-x-2 pt-2">
+          <Image
+            src={authorAvatar}
+            width={50}
+            height={50}
+            style={{ borderRadius: "50%" }}
+            alt={authorAvatar}
+          />
+          <div className="flex flex-col">
+            <p>{author}</p>
+            <p>{createTime}</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
