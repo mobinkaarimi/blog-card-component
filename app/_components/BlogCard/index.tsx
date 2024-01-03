c35db1b5e65fcd9e3a6e5a73000b7b3b7660de4d
mobinkaarimi <68745874+mobinkaarimi@users.noreply.github.com>
2024-01-03 21:06:15 +0330

    update[BlogCard]-> import BlogCardList and use it also write a function for map on blogCardList

--- app/_components/BlogCard/index.tsx
+++ app/_components/BlogCard/index.tsx
@@ -1,8 +1,28 @@
+import { cardItemType } from "@app/@types/cardItem";
+import BlogCardItem from "./BlogCardItem";
+import { blogCardList } from "@app/_constants";
+
 export default function BlogCard() {
 
+  function blogCard(_el: cardItemType): any {
+    return (
+      <div key={_el.id}>
+        <BlogCardItem
+          id={_el.id}
+          title={_el.title}
+          description={_el.description}
+          category={_el.category}
+          author={_el.author}
+          createTime={_el.createTime}
+          picture={_el.picture}
+          authorAvatar={_el.authorAvatar}
+        />
+      </div>
+    );
+  }
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-4 gap-x-6 gap-y-8">
-      Blog List
-    </div>
+    {blogCardList.map(blogCard)}
+  </div>
   )
 }
